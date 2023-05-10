434f80658d5af24e62146cf6bf4d076a04ab2bf2
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2023-05-10 08:39:36 +0300

    Start

---
+++ source/search.js
@@ -0,0 +1,31 @@
+function serch() {
+  var ser = document.getElementById("search").value.toLowerCase().replaceAll(" ","").replaceAll(".","").replaceAll(",","").replaceAll("-","");
+ 
+  
+  if(ser=="test"){
+		window.open("test_site.html");}
+  else if(ser=="null"){window.open("null.html");}
+  else if(ser=="nil"){window.open("null.html");}
+  else if(ser=="openprojectsource"){window.open("openprojectsource.html");}
+  else if(ser=="skeletonparty"){window.open("skull-party!.html");}
+  else if(ser=="woowzsite1999"){window.open("site_woowz11_2.html");}
+  else if(ser=="woowzsite1997"){window.open("site_woowz11.html");}
+  else if(ser=="list"){window.open("list.html");}
+  else if(ser=="all"){window.open("list.html");}
+  else if(ser=="allsites"){window.open("list.html");}
+  else if(ser=="allwebsites"){window.open("list.html");}
+  else if(ser=="firstpage"){window.open("firstpage.html");}
+  else if(ser=="firstsite"){window.open("firstpage.html");}
+  else if(ser=="firstwebsite"){window.open("firstpage.html");}
+  else if(ser=="dexon"){window.open("dexonmusic.html");}
+  else if(ser=="dexonmusic"){window.open("dexonmusic.html");}
+  else if(ser=="bloodeye"){window.open("blood_eye_co.html");}
+  else if(ser=="bloodeyenew"){window.open("blood_eye_co_new.html");}
+  else if(ser=="openprojectsourcedownload"){window.open("openprojectsource_download.html");}
+  else if(ser=="")
+  {
+		window.open("null.html");
+  }
+  
+
+}
