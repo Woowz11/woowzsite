55acff737b3e0a425511554629664bf8de0e02da
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-02-05 00:07:35 +0300

    я бы как бы да

--- source/lu_wiki/api.js
+++ source/lu_wiki/api.js
@@ -294,14 +294,14 @@ end)</code>`
 		"description": "Округляет число 'Число'",
 		"desmos": "9h9uuyruyw",
 		"example":
-`<code>Print(Floor(0))</code> Выводит 0
-<code>Print(Floor(1))</code> Выводит 1
-<code>Print(Floor(0.2))</code> Выводит 0
-<code>Print(Floor(0.7))</code> Выводит 1
-<code>Print(Floor(0.5))</code> Выводит 1
-<code>Print(Floor(-0.5))</code> Выводит 0
-<code>Print(Floor(1.5))</code> Выводит 2
-<code>Print(Floor(-1.5))</code> Выводит -1`
+`<code>Print(Round(0))</code> Выводит 0
+<code>Print(Round(1))</code> Выводит 1
+<code>Print(Round(0.2))</code> Выводит 0
+<code>Print(Round(0.7))</code> Выводит 1
+<code>Print(Round(0.5))</code> Выводит 1
+<code>Print(Round(-0.5))</code> Выводит -0
+<code>Print(Round(1.5))</code> Выводит 2
+<code>Print(Round(-1.5))</code> Выводит -1`
 	},{
 		"name": "RoundLower",
 		"params": [["Число","d"]],
@@ -309,14 +309,14 @@ end)</code>`
 		"description": "Округляет число 'Число' (если число равно 0.5, то вернёт 0)",
 		"desmos": "9h9uuyruyw",
 		"example":
-`<code>Print(Floor(0))</code> Выводит 0
-<code>Print(Floor(1))</code> Выводит 1
-<code>Print(Floor(0.2))</code> Выводит 0
-<code>Print(Floor(0.7))</code> Выводит 1
-<code>Print(Floor(0.5))</code> Выводит 0
-<code>Print(Floor(-0.5))</code> Выводит -1
-<code>Print(Floor(1.5))</code> Выводит 1
-<code>Print(Floor(-1.5))</code> Выводит -2`
+`<code>Print(RoundLower(0))</code> Выводит 0
+<code>Print(RoundLower(1))</code> Выводит 1
+<code>Print(RoundLower(0.2))</code> Выводит 0
+<code>Print(RoundLower(0.7))</code> Выводит 1
+<code>Print(RoundLower(0.5))</code> Выводит 0
+<code>Print(RoundLower(-0.5))</code> Выводит -1
+<code>Print(RoundLower(1.5))</code> Выводит 2
+<code>Print(RoundLower(-1.5))</code> Выводит -2`
 	},{
 		"name": "Trunc",
 		"params": [["Число","d"]],
@@ -409,17 +409,19 @@ end)</code>`
 		"name": "ToString",
 		"params": [["Цель","o"]],
 		"return": ["Результат","s"],
-		"description": "Конвертирует 'Цель' в строку",
-		"example": `<code>Print(ToString(true).." | "..ToString(2).." | "..ToString(function() end))</code>`
+		"description":
+`Конвертирует 'Цель' в строку
+При конвертации строки сохраняет ", а таблицу автоматически применяет Table:ToString`,
+		"example": `<code>Print(ToString(true).." | "..ToString(2).." | "..ToString("Example string!"))</code>`
 	},{
-		"name": "TableToString",
+		"name": "Table:ToString",
 		"params": [["Таблица","t"]],
 		"return": ["Результат","s"],
 		"description": "Конвертирует 'Таблица' в детальную строку таблицы",
 		"example":
 `<code>local t = {"Hello!","Bye!",{true,-6.21,Vector2(80,-10.77),{{"A","B","C"},{"C","B","A"}}},{}}
 Print(t)
-PrintFast(TableToString(t))</code>
+PrintFast(Table:ToString(t))</code>
 
 <code>Table{4}
 
@@ -459,7 +461,7 @@ PrintFast(TableToString(t))</code>
 	["EMPTY"] = {{{{},{}},{{},{}}},{{{},{}},{{},{}}}}
 }
 Print(t2)
-PrintFast(TableToString(t2))</code>
+PrintFast(Table:ToString(t2))</code>
 
 <code>Table{5}
 
@@ -808,6 +810,22 @@ end)</code>`
 	PrintFast(GameObject:GetName(i))
 end)</code>`
 	},{
+		"name": "Game:GameObjectDeleted",
+		"type": "event",
+		"params": [["Функция","f",[["Игровой объект","go"]]]],
+		"description": "Вызывает 'Функция' каждый раз, когда игровой объект удалён",
+		"example": 
+`<code>Game:GameObjectDeleted(function(i)
+	Print(GameObject:GetName(i).." removed!")
+end)
+
+local obj1 = GameObject:Create("OBJ1")
+local obj2 = GameObject:Create("OBJ2")
+local obj3 = GameObject:Create("OBJ3")
+
+GameObject:Delete(obj1)
+GameObject:Delete(obj3)</code>`
+	},{
 		"name": "Game:SetSimulationSpeed",
 		"params": [["Скорость симуляции","d"]],
 		"description": "Устанавливает 'Скорость симуляции'",
@@ -881,6 +899,42 @@ Print(GameObject:GetName(i))
 GameObject:Delete(i)
 Print(GameObject:GetName(i))</code>`
 	},{
+		"name": "GameObject:MousePressed",
+		"params": [["Цель","go"],["Функция","f",[["Клавиша","i"],["Объект к которому привязан ивент","go"]]]],
+		"description": "Вызывает 'Функция' каждый раз, когда на 'Цель' нажали мышкой",
+		"type": "event",
+		"example":
+`<code>local i = GameObject:Create("Left",GO_UI)
+GameObject:SetCenter(i,Vector2(1,0))
+GameObject:SetColor(i,Color(1))
+GameObject:SetPosition(i,Vector2(-0.6,0))
+GameObject:MousePressed(i,function(Key, Orig)
+	Print("Mouse press ["..Key.."] to ["..GameObject:GetName(Orig).."] #Left#")
+end)
+
+local i = GameObject:Create("Right",GO_UI)
+GameObject:SetCenter(i,Vector2(-1,0))
+GameObject:SetColor(i,Color(0,1))
+GameObject:SetPosition(i,Vector2(0.6,0))
+GameObject:MousePressed(i,function(Key, Orig)
+	Print("Mouse press ["..Key.."] to ["..GameObject:GetName(Orig).."] #Right#")
+end)</code>`
+	},{
+		"name": "GameObject:MouseHover",
+		"params": [["Цель","go"],["Функция","f",[["Наведён?","b"],["Объект к которому привязан ивент","go"]]]],
+		"description": "Вызывает 'Функция' каждый раз, когда на 'Цель' наведена мышка, или отведена",
+		"type": "event",
+		"example":
+`<code>local i = GameObject:Create("Hover test", GO_UI)
+GameObject:SetColor(i,Color(1))
+GameObject:MouseHover(i,function(Hover,Orig)
+	if (Hover) then
+		GameObject:SetColor(Orig,Color(0,1))
+	else
+		GameObject:SetColor(Orig,Color(1))
+	end
+end)</code>`
+	},{
 		"name": "GameObject:SetPosition",
 		"params": [["Цель","go"],["Позиция","v2"]],
 		"description": "Устанавливает 'Позиция' объекту 'Цель'",
@@ -898,6 +952,15 @@ end)</code>`
 		"example":
 `<code>local Obj = GameObject:Create(nil, GO_UI)
 GameObject:SetResize(Obj,false)</code>`
+	},{
+		"name": "GameObject:SetStatic",
+		"params": [["Цель","go"],["Сделать статичным?","b"]],
+		"description":
+`Устанавливает 'Сделать статичным?' объекту 'Цель'
+Работает только на GO_Physical`,
+		"example":
+`<code>local i = GameObject:Create("Static GameObject", GO_Physical)
+GameObject:SetStatic(i,true)</code>`
 	},{
 		"name": "GameObject:MakeItText",
 		"params": [["Цель","go"],["Стартовый текст","s"]],
@@ -967,6 +1030,53 @@ for a = 0, linesize do
 	GameObject:SetPosition(i, Vector2((a-(linesize/2))*2,0))
 	GameObject:SetColor(i, Color(1,IfThen(a%2 == 0, 1-a/linesize,1),IfThen(a%3 == 0, 1-a/linesize,1)))
 end</code>`
+	},{
+		"name": "GameObject:SetText",
+		"params": [["Цель","go"],["Текст","s"]],
+		"description": "Устанавливает 'Текст' объекту 'Цель'",
+		"example":
+`<code>local i = GameObject:Create("Time",GO_UI)
+local TimePrefix = "Time: "
+local t = 0
+GameObject:MakeItText(i,TimePrefix..t)
+Game:Update(function()
+	t = t + 1
+	GameObject:SetText(i,TimePrefix..t)
+end)</code>`
+	},{
+		"name": "Table:Remove",
+		"params": [["Таблица","t"],["Ключ (или индекс)","o"],["Удалять по ключу?","b"]],
+		"description":
+`Удаляет из 'Таблица' элемент под индексом 'Ключ (или индекс)' если 'Удалять по ключу?' равен false или nil, или
+удаляет ключ 'Ключ (или индекс)' из 'Таблица' если 'Удалять по ключу?' равен true`,
+		"example":
+`<code>local T = {10,9,8,7,6,5,4,3,2,1,0}
+Print(Table:ToString(T))
+Table:Remove(T,2)
+Table:Remove(T,2)
+Print(Table:ToString(T))
+
+T = {[true] = 1, [false] = 2, [1] = 3, ["Text!"] = 4, [{1,2,3}] = 5}
+Print(Table:ToString(T))
+Table:Remove(T,false,true)
+Table:Remove(T,"Text!",true)
+Print(Table:ToString(T))</code>`
+	},{
+		"name": "Table:Pairs",
+		"params": [["Таблица","t"],["Функция","f",[["Индекс","i"],["Ключ","o"],["Переменная","o"]]]],
+		"description": `Разбирает 'Таблица' по компонентам (индекс (позиция элемента), ключ, переменная) и вызывает их в 'Функция',`,
+		"example":
+`<code>local T = {"a","b","c","d","f","g"}
+
+Table:Pairs(T,function(i,Key,Value)
+	Print("["..i.."] "..Key.." = "..ToString(Value))
+end)
+
+T = {["a"] = true, ["b"] = false, ["c"] = Vector2(0,2), ["d"] = "str"}
+
+Table:Pairs(T,function(i,Key,Value)
+	Print("["..i.."] "..Key.." = "..ToString(Value))
+end)</code>`
 	},{
 		"name": "GameObject:GetName",
 		"params": [["Цель","go"]],
@@ -1335,7 +1445,7 @@ Print(C) Print(C.A)</code> Выводит Color(1,1,1,0) и 0`
 const Const = {
 	"Keys": {
 		"Mouse": [["KEY_MOUSE_LEFT",0],["KEY_MOUSE_RIGHT",1],["KEY_MOUSE_MIDDLE",2]],
-		"Keyboard": [["KEY_SPACE",32],["KEY_APOSTROPHE",39],["KEY_COMMA",44],["KEY_MINUS",45],["KEY_PERIOD",46],["KEY_SLASH",47],["KEY_0",48],["KEY_1",49],["KEY_2",50],["KEY_3",51],["KEY_4",52],["KEY_5",53],["KEY_6",54],["KEY_7",55],["KEY_8",56],["KEY_9",57],["KEY_SEMICOLON",59],["KEY_EQUAL",61],["KEY_A",65],["KEY_B",66],["KEY_C",67],["KEY_D",68],["KEY_E",69],["KEY_F",70],["KEY_G",71],["KEY_H",72],["KEY_I",73],["KEY_J",74],["KEY_K",75],["KEY_L",76],["KEY_M",77],["KEY_N",78],["KEY_O",79],["KEY_P",80],["KEY_Q",81],["KEY_R",82],["KEY_S",83],["KEY_T",84],["KEY_U",85],["KEY_V",86],["KEY_W",87],["KEY_X",88],["KEY_Y",89],["KEY_Z",90],["KEY_LEFT_BRACKET",91],["KEY_BACKSLASH",92],["KEY_RIGHT_BRACKET",93],["KEY_GRAVE_ACCENT",96],["KEY_WORLD_1",161],["KEY_WORLD_2",162],["KEY_ESCAPE",256],["KEY_ENTER",257],["KEY_TAB",258],["KEY_BACKSPACE",259],["KEY_INSERT",260],["KEY_DELETE",261],["KEY_RIGHT",262],["KEY_LEFT",263],["KEY_DOWN",264],["KEY_UP",265],["KEY_PAGE_UP",266],["KEY_PAGE_DOWN",267],["KEY_HOME",268],["KEY_END",269],["KEY_CAPS_LOCK",280],["KEY_SCROLL_LOCK",281],["KEY_NUM_LOCK",282],["KEY_PRINT_SCREEN",283],["KEY_PAUSE",284],["KEY_F1",290],["KEY_F2",291],["KEY_F3",292],["KEY_F4",293],["KEY_F5",294],["KEY_F6",295],["KEY_F7",296],["KEY_F8",297],["KEY_F9",298],["KEY_F10",299],["KEY_F11",300],["KEY_F12",301],["KEY_F13",302],["KEY_F14",303],["KEY_F15",304],["KEY_F16",305],["KEY_F17",306],["KEY_F18",307],["KEY_F19",308],["KEY_F20",309],["KEY_F21",310],["KEY_F22",311],["KEY_F23",312],["KEY_F24",313],["KEY_F25",314],["KEY_KP_0",320],["KEY_KP_1",321],["KEY_KP_2",322],["KEY_KP_3",323],["KEY_KP_4",324],["KEY_KP_5",325],["KEY_KP_6",326],["KEY_KP_7",327],["KEY_KP_8",328],["KEY_KP_9",329],["KEY_KP_DECIMAL",330],["KEY_KP_DIVIDE",331],["KEY_KP_MULTIPLY",332],["KEY_KP_SUBTRACT",333],["KEY_KP_ADD",334],["KEY_KP_ENTER",335],["KEY_KP_EQUAL",336],["KEY_LEFT_SHIFT",340],["KEY_LEFT_CONTROL",341],["KEY_LEFT_ALT",342],["KEY_LEFT_SUPER",343],["KEY_RIGHT_SHIFT",344],["KEY_RIGHT_CONTROL",345],["KEY_RIGHT_ALT",346],["KEY_RIGHT_SUPER",347],["KEY_MENU",348]]
+		"Keyboard": [["KEY_SPACE",32],["KEY_APOSTROPHE",39],["KEY_COMMA",44],["KEY_MINUS",45],["KEY_PERIOD",46],["KEY_SLASH",47],["KEY_0",48],["KEY_1",49],["KEY_2",50],["KEY_3",51],["KEY_4",52],["KEY_5",53],["KEY_6",54],["KEY_7",55],["KEY_8",56],["KEY_9",57],["KEY_SEMICOLON",59],["KEY_EQUAL",61],["KEY_A",65],["KEY_B",66],["KEY_C",67],["KEY_D",68],["KEY_E",69],["KEY_F",70],["KEY_G",71],["KEY_H",72],["KEY_I",73],["KEY_J",74],["KEY_K",75],["KEY_L",76],["KEY_M",77],["KEY_N",78],["KEY_O",79],["KEY_P",80],["KEY_Q",81],["KEY_R",82],["KEY_S",83],["KEY_T",84],["KEY_U",85],["KEY_V",86],["KEY_W",87],["KEY_X",88],["KEY_Y",89],["KEY_Z",90],["KEY_LEFT_BRACKET",91],["KEY_BACKSLASH",92],["KEY_RIGHT_BRACKET",93],["KEY_GRAVE_ACCENT",96],["KEY_WORLD_1",161],["KEY_WORLD_2",162],["KEY_ESCAPE",256],["KEY_ENTER",257],["KEY_TAB",258],["KEY_BACKSPACE",259],["KEY_INSERT",260],["KEY_DELETE",261],["KEY_RIGHT",262],["KEY_LEFT",263],["KEY_DOWN",264],["KEY_UP",265],["KEY_PAGE_UP",266],["KEY_PAGE_DOWN",267],["KEY_HOME",268],["KEY_END",269],["KEY_CAPS_LOCK",280],["KEY_SCROLL_LOCK",281],["KEY_NUM_LOCK",282],["KEY_PRINT_SCREEN",283],["KEY_PAUSE",284],["KEY_F1",290],["KEY_F2",291],["KEY_F3",292],["KEY_F4",293],["KEY_F5",294],["KEY_F6",295],["KEY_F7",296],["KEY_F8",297],["KEY_F9",298],["KEY_F10",299],["KEY_F11",300],["KEY_F12",301],["KEY_F13",302],["KEY_F14",303],["KEY_F15",304],["KEY_F16",305],["KEY_F17",306],["KEY_F18",307],["KEY_F19",308],["KEY_F20",309],["KEY_F21",310],["KEY_F22",311],["KEY_F23",312],["KEY_F24",313],["KEY_F25",314],["KEY_K0",320],["KEY_K1",321],["KEY_K2",322],["KEY_K3",323],["KEY_K4",324],["KEY_K5",325],["KEY_K6",326],["KEY_K7",327],["KEY_K8",328],["KEY_K9",329],["KEY_K_DECIMAL",330],["KEY_K_DIVIDE",331],["KEY_K_MULTIPLY",332],["KEY_K_SUBTRACT",333],["KEY_K_ADD",334],["KEY_K_ENTER",335],["KEY_K_EQUAL",336],["KEY_LEFT_SHIFT",340],["KEY_LEFT_CONTROL",341],["KEY_LEFT_ALT",342],["KEY_LEFT_SUPER",343],["KEY_RIGHT_SHIFT",344],["KEY_RIGHT_CONTROL",345],["KEY_RIGHT_ALT",346],["KEY_RIGHT_SUPER",347],["KEY_MENU",348]]
 	},
 	
 	"Errors": [
@@ -1358,6 +1468,11 @@ const Const = {
 		["GO_Default",0,"Обычный тип объекта, пустышка"],
 		["GO_UI",1,"Объект является интерфейсом"],
 		["GO_Physical",2,"Объект имеет физику"]
+	],
+	
+	"ColliderTypes": [
+		["COL_Square",1,"Квадратный коллайдер"],
+		["COL_Circle",2,"Круглый коллайдер"]
 	],
 	
 	"Math": [
