71846a30334b4949ea6585da63cfbc1d5be1dac9
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-06-15 00:22:41 +0300

    сифилис 🤩

--- source/source.js
+++ source/source.js
@@ -40,7 +40,8 @@ WoowzsiteSourceRandomSite = AddTableToTable(AddTableToTable(AddFullnessToTable([
 	"fakereal","fish_","i'm only 1 year-old!","piss_ploho","error00000","ha-tot-samiy-mod","good-day","aqua-dog","again-real-of-fake","chika",
 	"cacaat","omg..","out of memory!","squid","mabkrosm","worms revolution","daty","mine2","hororr","s","neighbor_dog","teftela","le-Lego","coockie",
 	"PAVEL","e6222ea2-92f7-4005-a436-289a9370e03b","37171e31-de56-4637-a31c-8c0b1c0bd4db","horny","Z","brin","tttttttttt","tttt","wwwwwe","dla-slaboymnogo-normalno",
-	"ass","Beispiel_System32_Ordner_mit_DLL_Dateie_2020-10-12-16-07-26","lob","heartbeat","real","opengl"
+	"ass","Beispiel_System32_Ordner_mit_DLL_Dateie_2020-10-12-16-07-26","lob","heartbeat","real","opengl","hackman","mine-mine","comic","assembly",
+	"img-GlWt4J","no","niga","pro","doudou2","think-about-this","reality","we_know_you","image0","kiki","mops"
 ],"source/specialforrandomsite/",".png"),AddFullnessToTable([
 	"swag","whasapa2","hq720","images","8f5b846b77ca5e3d6cf4cae08416c31a","20250328_094038","A83-0825","tragedy-rule-v0-mb8il71vtqze1"
 ],"source/specialforrandomsite/",".jpg")),AddFullnessToTable([
@@ -49,7 +50,8 @@ WoowzsiteSourceRandomSite = AddTableToTable(AddTableToTable(AddFullnessToTable([
 	"a_71d80d5021271963722772d663bdb5df-1-1","GIF 17.08.2020 19-25-55","gg1983e7f015","cat","teapottutor)))","moss","toletole","idiot","bear",
 	"ai1","ai2","ai3","ai4","le_pedo","tresh","ezgif-5-8da35159bb","popa","horse","megadoff","megadoff2","megadoo33333","result","image0","explode-please",
 	"woah","SPOILER_more","twitter_1895217911683522717","64ac0bf08aa29b4f","torch","ezgif-2-baf276abda","hyha","help-yeah","Lets_take_a_look_off","hotline-miami","c6j0PNI",
-	"spongebob-walking","prosto-chydo"
+	"spongebob-walking","prosto-chydo","CgsUwNSl","you","yes","frog","xuL4tZV0R_VlZcdWIw_rsfpl57383PjBsLx3jTZt9bDf7BV68Px8dYJBsUWhtKUV3J2IO5GaML5hYmw__2BOC1E2","image1",
+	"papa","sticker","emotion","semen_dead_body","you_megapocus"
 ],"source/specialforrandomsite/",".gif"))
 
 WoowzsiteSourceImagesJPG = AddFullnessToTable([
@@ -81,7 +83,7 @@ WoowzsiteSourceVideos = AddTableToTable( AddFullnessToTable([
 	"4013a8098ec8d11c48507fceb1dc164e","round_2025-02-22_14-37-16","wewewe","Touch that thang n it’ll explode","VID_20250324_162427_266","7767167994467_1","Hour",
 	"VID_20240920_091136_169","VID_20240831_124633_331","Ce3nlNsYVross4Of.mov","video_2024-08-30_00-39-33","89355f399e8b6ce2","7022843660899","SPOILER_video_2024-11-30_20-04-30",
 	"a13683105540c28a","video_2024-12-03_00-53-42","winir","postthis","fly","horse","the-chewer-chew","forsaken gameplay","memeee","spongebob-spongebob-meme",
-	"79","fDyo4gl","ia-slomay-tebe-palchiki"
+	"79","fDyo4gl","ia-slomay-tebe-palchiki","RDT_20250612_112251","shadertoy","Джоник_Македонский_Слушай_ты_хакер_Миша_из_Питера_Russian_dubbed"
 ],"source/specialforrandomsite/",".mp4"), AddFullnessToTable([
 	"ssstik.io_illegalbugtrader_1725631254849","LGqqu1KcC9ZCLiN7","IE28099m20the20dog20on20the20first20slide","egg","air_strike","6uENJ1JOvxyX6mzZ","staris-tiktoke","shutip","ser_na_zemly","ptita",
 	"oh_my_god","4949701782008910617","cachedVideo","123456789","1BE3B84A-3023-41B4-9CA0-331F29246BA0"
@@ -89,7 +91,8 @@ WoowzsiteSourceVideos = AddTableToTable( AddFullnessToTable([
 
 WoowzsiteSourceAudios = AddTableToTable(AddTableToTable(AddFullnessToTable([
 	"ВЫЙТИ НА УЛИЦУ ТАМ ЙОБАНЫ ЛОС","spongebob-asking-are-you-pooping-made-with","Morning-Flower","s","Cheetahmen","Toilet-story","Mr. Trololo original upload","idiotic music","otkroi-glazki",
-	"寻梦高粱红","Qahwah.Bootleg (feat. ilyhiryu)","gabber.bootleg","spo","what-the-song1","what-the-song2","what-the-song3","It's PINEAPPLE piizza tami(1)"
+	"寻梦高粱红","Qahwah.Bootleg (feat. ilyhiryu)","gabber.bootleg","spo","what-the-song1","what-the-song2","what-the-song3","It's PINEAPPLE piizza tami(1)","28_M_Necro","NO",
+	"TROLL_HIT_4"
 ],"source/specialforrandomsite/",".mp3"),AddFullnessToTable([
 	"doom","Schoolhouse_Trouble_Metal_Version","Pixelizer","sun","babah","The Daniel Pemberton TV Orchestra - The Orb Of Dreamers","forest","button","bass","dentist","open"
 ],"source/",".mp3")),[
@@ -356,7 +359,8 @@ function GetAllWoowzsiteImages(){
 	var tabl = GetAllWoowzsite();
 	var tabl_ = [];
 	for(var src of tabl){
-		if(src.includes(".png")||src.includes(".jpg")||src.includes(".gif")){
+		src = src.toLowerCase();
+		if(src.includes(".png")||src.includes(".jpg")||src.includes(".jpeg")||src.includes(".gif")||src.includes(".webp")){
 			tabl_.push(src);
 		}
 	}
@@ -367,6 +371,7 @@ function GetAllWoowzsiteIcons(){
 	var tabl = GetAllWoowzsite();
 	var tabl_ = [];
 	for(var src of tabl){
+		src = src.toLowerCase();
 		if(src.includes(".ico")){
 			tabl_.push(src);
 		}
@@ -445,4 +450,21 @@ WoowzsiteWords = ["растворился","арбуз","пёрни","троти
 "😊","©","😁","☺","😋","😇","☢","☣","☕ <- JAVA","☨","■","♻","Lithium Universe","Bloodraw","Blood Eye","13:32","c++","PYTHON ГОВНО!!!","сфд","superfighters deluxe","Survival Tools","Extra Backrooms","WVirus","💢","₿",
 "₽","㎏","جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد  جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد من جديد!الشيوعية ستولد :woowz_bloodeye_gold_toilet: 🤬 🤯 من جديد!الشيوعية ستولد  جديد!الشيوعية ستولد من جديد!الشيوعية ستولد 🤟",
 "VNNet запрашивает новые разрешения⚠","сын сутенёра","ау","ауууу","асемблер","git","характеризуй себя","сыновья","барак","хули","аеро","эхо","войны","драйвер","драйвера","opengl","OpenGL","Vulkan","iLovePDF","assembler","NASM","ассемблер","G-Код","чпу","сраный спанч","крайбёрд","литуизм","байт","бит","буфер","Box2D","JBullet","MCP","JDK","JRE","os.flp",
-"манифест","писечник","писечник тв","артём","отец писечника","гулу","GOLU","😁😁😁😁"]
+"манифест","писечник","писечник тв","артём","отец писечника","гулу","GOLU","😁😁😁😁","﷽",
+`32141ツ [создатель] Только что (не)
+32141ツ [создатель] Только что (не)
+🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨LET'S GO
+🟨🟨⬛⬛⬛🟨🟨🟨⬛⬛⬛🟨🟨SRAN-SPONGE
+🟨⬛⬛⬜⬛⬛🟨⬛⬛⬜⬛⬛🟨TAKE
+🟨⬛⬜⬛⬜⬛🟨⬛⬜⬛⬜⬛🟨YOU'R LEGAL
+🟨⬛⬛⬜⬛⬛🟨⬛⬛⬜⬛⬛🟨SOUL
+🟨🟨⬛⬛⬛🟨🟨🟨⬛⬛⬛🟨🟨pleas&))) 🙏 
+🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨{WORKSHOP}
+🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨
+🟨🟨🟨🟨⬛⬛⬛⬛⬛🟨🟨🟨🟨
+🟨🟨🟨⬛🟨⬜🟨⬜🟨⬛🟨🟨🟨
+🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨if... if you dont get him then. hes gonna to get you flesh.
+🟫🟫🟫🟫🟫⬜🟥⬜🟫🟫🟫🟫🟫
+⬛⬛🟫⬛⬛⬜🟥⬜⬛⬛🟫⬛⬛СРАНЫЙ СПАНЧ))))))))))) ТОТ САМЫЙ
+🟫🟫🟫🟫🟫🟫🟥🟫🟫🟫🟫🟫🟫ЗНАЕТЕ ТАКОГО??? НУ У ВАС ЗА ОКНОМ СТОИТ СЕЙЧАС)))))
+кастеризация ---- 🟥 ----- пастеризация`]
